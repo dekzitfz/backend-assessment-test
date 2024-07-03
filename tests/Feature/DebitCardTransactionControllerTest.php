2b1bfdb807cc1d55798ec981ebd99c8ec8530979
alfi-repo <9110279+alfi-repo@users.noreply.github.com>
2024-07-03 20:30:55 +0700

    test: customer can see a list of debit card transactions.

--- tests/Feature/DebitCardTransactionControllerTest.php
+++ tests/Feature/DebitCardTransactionControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\DebitCard;
+use App\Models\DebitCardTransaction;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Passport\Passport;
@@ -27,7 +28,15 @@ class DebitCardTransactionControllerTest extends TestCase
 
     public function testCustomerCanSeeAListOfDebitCardTransactions()
     {
+        $numberOfTransactions = 3;
+        $transactions = DebitCardTransaction::factory($numberOfTransactions)->for($this->debitCard)->create();
+
         // get /debit-card-transactions
+        $response = $this->getJson("api/debit-card-transactions?debit_card_id={$this->debitCard->id}");
+
+        $response->assertStatus(200)
+            ->assertJsonCount($numberOfTransactions)
+            ->assertJsonStructure(['*' => ['amount', 'currency_code']]);
     }
 
     public function testCustomerCannotSeeAListOfDebitCardTransactionsOfOtherCustomerDebitCard()
