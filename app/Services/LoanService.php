1cf15155ca767f4b9071e0a746353c3f736a79ba
alfi-repo <9110279+alfi-repo@users.noreply.github.com>
2024-07-04 09:05:00 +0700

    feat: implement create loan service.

--- app/Services/LoanService.php
+++ app/Services/LoanService.php
@@ -4,7 +4,11 @@ namespace App\Services;
 
 use App\Models\Loan;
 use App\Models\ReceivedRepayment;
+use App\Models\ScheduledRepayment;
 use App\Models\User;
+use Carbon\CarbonImmutable;
+use Illuminate\Support\Facades\DB;
+use Throwable;
 
 class LoanService
 {
@@ -18,10 +22,33 @@ class LoanService
      * @param  string  $processedAt
      *
      * @return Loan
+     * @throws Throwable
      */
     public function createLoan(User $user, int $amount, string $currencyCode, int $terms, string $processedAt): Loan
     {
-        //
+        DB::beginTransaction();
+        try {
+            $newLoan = Loan::create([
+                'user_id' => $user->id,
+                'amount' => $amount,
+                'terms' => $terms,
+                'outstanding_amount' => $amount,
+                'currency_code' => $currencyCode,
+                'processed_at' => $processedAt,
+                'status' => Loan::STATUS_DUE,
+            ]);
+
+            $newLoan->scheduledRepayments()->createMany(
+                $this->createPaymentScheduleList($newLoan)
+            );
+
+            DB::commit();
+        } catch (Throwable $th) {
+            DB::rollBack();
+            throw $th;
+        }
+
+        return $newLoan;
     }
 
     /**
@@ -37,5 +64,40 @@ class LoanService
     public function repayLoan(Loan $loan, int $amount, string $currencyCode, string $receivedAt): ReceivedRepayment
     {
         //
+    }
+
+
+    /**
+     * Create a payment schedule list with monthly intervals.
+     *
+     * @param Loan $loan
+     *
+     * @return array
+     */
+    protected function createPaymentScheduleList(Loan $loan): array
+    {
+        $schedules = [];
+        $totalDebt = $loan->amount;
+        $billEachTerm = intdiv($loan->amount, $loan->terms);
+        $scheduleBaseDate = CarbonImmutable::parse($loan->processed_at);
+
+        for ($i = 1; $i <= $loan->terms; $i++) {
+            // remaining debt are billed at the final payment.
+            if ($i === $loan->terms) {
+                $billEachTerm = $totalDebt;
+            }
+
+            $schedules[] = [
+                'amount' => $billEachTerm,
+                'outstanding_amount' => $billEachTerm,
+                'currency_code' => $loan->currency_code,
+                'due_date' => $scheduleBaseDate->addMonths($i)->format('Y-m-d'),
+                'status' => ScheduledRepayment::STATUS_DUE,
+            ];
+
+            $totalDebt -= $billEachTerm;
+        }
+
+        return $schedules;
     }
 }
