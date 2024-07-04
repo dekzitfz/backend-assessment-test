b396e58ed4688641af889f92327aa2c6920213d2
alfi-repo <9110279+alfi-repo@users.noreply.github.com>
2024-07-04 09:08:20 +0700

    feat: loan factory.

--- database/factories/LoanFactory.php
+++ database/factories/LoanFactory.php
@@ -21,8 +21,16 @@ class LoanFactory extends Factory
      */
     public function definition(): array
     {
+        $amount = $this->faker->numberBetween(1000, 2000);
+
         return [
-            // TODO: Complete factory
+            'user_id' => fn() => User::factory()->create(),
+            'terms' => $this->faker->randomNumber(1),
+            'amount' => $amount,
+            'outstanding_amount' => $amount,
+            'currency_code' => Loan::CURRENCY_SGD,
+            'processed_at' => $this->faker->dateTimeThisYear(),
+            'status' => Loan::STATUS_DUE,
         ];
     }
 }
