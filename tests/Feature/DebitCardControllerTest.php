884056288bca3c77da656385b070a8fa34b9b59a
Agus Salim <aguss6460@gmail.com>
2024-08-11 11:56:55 +0700

    test-feature: customer can deactive debit card

--- tests/Feature/DebitCardControllerTest.php
+++ tests/Feature/DebitCardControllerTest.php
@@ -124,7 +124,19 @@ class DebitCardControllerTest extends TestCase
 
     public function testCustomerCanDeactivateADebitCard()
     {
-        // put api/debit-cards/{debitCard}
+        $debitCard = DebitCard::factory()->active()->for($this->user)->create();
+        $payload = [
+            'is_active' => false,
+        ];
+
+        $response = $this->putJson("/api/debit-cards/{$debitCard->id}", $payload);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['id', 'number', 'type', 'expiration_date', 'is_active'])
+            ->assertJsonFragment(['is_active' => false]);
+
+        $debitCard->refresh();
+        $this->assertFalse($debitCard->is_active);
     }
 
     public function testCustomerCannotUpdateADebitCardWithWrongValidation()
