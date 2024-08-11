e62911cd1ebf4b0fda4ece003c28cff5e4f95bcf
Agus Salim <aguss6460@gmail.com>
2024-08-11 11:27:10 +0700

    fix: fixing database migration

--- database/migrations/2020_01_28_100002_create_received_repayments_table.php
+++ database/migrations/2020_01_28_100002_create_received_repayments_table.php
@@ -15,7 +15,7 @@ class CreateReceivedRepaymentsTable extends Migration
     {
         Schema::create('received_repayments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('loan_id');
+            $table->foreignId('loan_id');
 
             // TODO: Add missing columns here
 
